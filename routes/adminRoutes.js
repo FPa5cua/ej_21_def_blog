f7d50f65465df85f1daf628296a0e6711dfac9eb
pedroneg3 <pedroneg3@gmail.com>
2023-02-14 13:42:22 -0300

    destroy

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -9,8 +9,8 @@ router.get("/", adminController.index);
 router.get("/crear", adminController.create);
 router.post("/", adminController.store);
 router.get("/:id", adminController.show);
-router.get("/:id/editar", adminController.edit);
+router.get("/editar/:id", adminController.edit);
 router.get("/:id", adminController.update);
-router.get("/:id", adminController.destroy);
+router.get("/eliminar/:id", adminController.destroy);
 
 module.exports = router;
